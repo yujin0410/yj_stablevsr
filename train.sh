80983acbefe38aa1a972323885cb4aa2853e1dff
yujin <110644760+yujin0410@users.noreply.github.com>
2026-03-15 01:15:19 +0900

    Add training script for StableVSR model

---
+++ train.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+MODEL_ID='claudiom4sir/StableVSR'
+OUTPUT_DIR='experiments/my_exp_name_0313'
+GPUS="0 4"
+
+GPUS_STR=$(echo $GPUS | tr ' ' ',')
+
+export CUDA_VISIBLE_DEVICES=$GPUS_STR
+
+# Calculate the number of GPUs (i.e., the number of processes)
+NUM_PROCESSES=$(echo $GPUS | wc -w)
+
+accelerate launch --num_processes $NUM_PROCESSES --main_process_port 29501 train.py \
+ --pretrained_model_name_or_path=$MODEL_ID \
+ --pretrained_vae_model_name_or_path=$MODEL_ID \
+ --output_dir=$OUTPUT_DIR \
+ --dataset_config_path="/home/yjcho/StableVSR/dataset/config_reds.yaml" \
+ --learning_rate=5e-5 \
+ --validation_steps=100000 \
+ --train_batch_size=8 \
+ --dataloader_num_workers=8 \
+ --max_train_steps=20000 \
+ --enable_xformers_memory_efficient_attention \
+  --validation_image "/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000000.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000001.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000002.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000003.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000004.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000005.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000006.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000007.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000008.png;/home/yjcho/dataset/REDS/train/bicubic/X4/020/00000009.png" \
+  --validation_prompt ""
